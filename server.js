98246ac7b1fdfb0dc9f682aa0fc6c1366cbd9cb4
honglin <yanghl22@gmail.com>
2017-10-26 00:08:49 +0800

    minor change

--- server.js
+++ server.js
@@ -20,12 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(errorHandler);
 
-app.use(cors());
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
+app.use(cors());
+
 
 app.use(express.static(__dirname + '/static/images'));
 const routes = require('./api/routes/userRoute');
